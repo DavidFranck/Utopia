59862239db36f046cf02b95f4e731268667cc305
wanghh <david.wang@mobiexchanger.com>
2018-06-28 10:35:58 +0800

    fix

--- utopia/teststation/src/test/java/com/david/pattern/factory/BeanFactory.java
+++ utopia/teststation/src/test/java/com/david/pattern/factory/BeanFactory.java
@@ -2,23 +2,26 @@ package com.david.pattern.factory;
 
 import com.google.common.collect.Maps;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
-    private static final BeanFactory instance = new BeanFactory();
-    private static final Map<String, Class> classMap = Maps.newHashMap();
+    private static BeanFactory instance = new BeanFactory();
+    private static Map<String, Class> classMap;
 
     private BeanFactory() {
         init();
     }
 
     private void init() {
+        classMap = Maps.newConcurrentMap();
         Set<Class<?>> classes = ClassHelper.getClasses(Main.class.getPackage().getName());
         classes.forEach(c -> {
             Bean bean = c.getAnnotation(Bean.class);
             if (Objects.nonNull(bean)) {
+                System.out.println(bean.value());
                 classMap.put(bean.value(), c);
             }
         });
@@ -34,7 +37,7 @@ public class BeanFactory {
         return null;
     }
 
-    public static <T> T getBean(String name, Class<T> clazz) throws IllegalAccessException, InstantiationException {
+    public <T> T getBean(String name, Class<T> clazz) throws IllegalAccessException, InstantiationException {
         Class<T> c = classMap.get(name);
         return c.newInstance();
     }
--- utopia/teststation/src/test/java/com/david/pattern/factory/Main.java
+++ utopia/teststation/src/test/java/com/david/pattern/factory/Main.java
@@ -1,8 +1,13 @@
 package com.david.pattern.factory;
 
+import org.junit.Assert;
+
 public class Main {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-//        BeanFactory.getInstance().getBean("aa",Circle.class);
-//        System.out.println(Main.class.getPackage().getName());
+        Circle circle = BeanFactory.getInstance().getBean("circle", Circle.class);
+        circle.draw();
+        Square square = BeanFactory.getInstance().getBean("square", Square.class);
+        square.draw();
+//        Square error = BeanFactory.getInstance().getBean("1square", Square.class);
     }
 }
