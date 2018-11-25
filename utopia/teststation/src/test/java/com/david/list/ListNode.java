8a5f33e19f0d405f7bbb9e3ddb8cf8cba032068c
wanghh <david.wang@mobiexchanger.com>
2018-11-25 18:13:16 +0800

    链表合并

--- utopia/teststation/src/test/java/com/david/list/ListNode.java
+++ utopia/teststation/src/test/java/com/david/list/ListNode.java
@@ -1,6 +1,58 @@
 package com.david.list;
 
 public class ListNode {
+    //合并两个有序链表从小到大 递归
+    public static ListNode merge1(ListNode head1, ListNode head2) {
+        if (head1 == null && head2 == null) {
+            return null;
+        }
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+        ListNode head;//新的头结点
+        if (head1.value > head2.value) {//小的在前面
+            head = head2;
+            head.next = merge1(head1, head2.next);//递归吧后面节点的next赋值
+        } else {
+            head = head1;
+            head.next = merge1(head1.next, head2);
+        }
+        return head;
+    }
+
+    //合并两个有序链表从小到大 非递归
+    public static ListNode merge2(ListNode head1, ListNode head2) {
+        if (head1 == null && head2 == null) {
+            return null;
+        }
+        ListNode head = head1.value < head2.value ? head1 : head2;//取小的放在head
+        ListNode cur1 = head == head1 ? head1 : head2;//被合并链表 初始值为head
+        ListNode cur2 = head == head1 ? head2 : head2;//合并链表
+
+        ListNode pre = null;//curr1前一个
+        ListNode next = null;//curr2后一个
+
+        while (cur1 != null && cur2 != null) {
+            //第一个必定是cur1
+            if (cur1.value <= cur2.value) {
+                pre = cur1;
+                cur1 = cur1.next;
+            } else {
+                next = cur2.next;//保存cur2下一个
+                pre.next = cur2;//填充cur2到cur1
+                cur2.next = cur1;//链表插入
+                pre = cur2;
+                cur2 = next;//遍历
+            }
+        }
+        //最后一个
+        pre.next = cur1 == null ? cur2 : cur1;
+        return head;
+    }
+
     //遍历
     public static void traverse(ListNode head) {
         while (head != null) {
@@ -12,8 +64,8 @@ public class ListNode {
 
     //反转
     public static ListNode reverse(ListNode head) {
-        ListNode pre = null;
-        ListNode next;
+        ListNode pre = null;//当前节点的上一个
+        ListNode next;//当前节点的下一个节点
         while (head != null) {
             next = head.next;
             head.next = pre;//反转链表
--- utopia/teststation/src/test/java/com/david/list/TestLinkedList.java
+++ utopia/teststation/src/test/java/com/david/list/TestLinkedList.java
@@ -2,18 +2,39 @@ package com.david.list;
 
 import org.junit.Test;
 
+import static com.david.list.ListNode.merge1;
+import static com.david.list.ListNode.merge2;
+
 public class TestLinkedList {
 
     @Test
     public void testLinkedList() {
-        final ListNode listNode1 = new ListNode(1);
-        final ListNode listNode2 = new ListNode(2);
-        final ListNode listNode3 = new ListNode(3);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        ListNode.traverse(listNode1);
-        ListNode.reverse(listNode1);//反转后3位头结点
-        ListNode.traverse(listNode3);
-        System.out.println(ListNode.mid(listNode3));
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+
+
+//        listNode1.next = listNode2;
+//        listNode2.next = listNode3;
+//        listNode5.next = listNode4;
+
+        listNode1.next = listNode3;
+        listNode2.next = listNode5;
+
+        ListNode head = listNode1;
+        ListNode head2 = listNode2;
+
+
+//        ListNode.traverse(head);
+//        head = ListNode.reverse(head);//反转后3位头结点
+//        ListNode.traverse(head);
+//        System.out.println(ListNode.mid(head));
+
+
+        ListNode listNode = merge2(head, head2);
+        ListNode.traverse(listNode);
     }
 }
