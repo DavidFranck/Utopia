853c36855d539e1961ca3b94315cb5e915b60eb3
hehan.wang <hehan.wang@happyelements.com>
2020-07-03 23:21:55 +0800

    [add]merge two list

---
+++ utopia/teststation/src/test/java/com/david/leetcode/Question21.java
@@ -0,0 +1,61 @@
+package com.david.leetcode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question21 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode res = new Solution().mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.print(res.val + " --> ");
+            res = res.next;
+        }
+    }
+
+    static public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //递归法 头结点小的链表 next为另一个链表
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            //空的话把其余那个链表接在后面
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            //返回头结点小的链表
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+}
