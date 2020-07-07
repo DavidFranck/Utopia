48d284c3c64675fc482dbcb65fbc972b56bb3062
hehan.wang <hehan.wang@happyelements.com>
2020-07-07 23:12:29 +0800

    [add]

--- utopia/teststation/src/test/java/com/david/leetcode/Question2.java
+++ utopia/teststation/src/test/java/com/david/leetcode/Question2.java
@@ -1,7 +1,5 @@
 package com.david.leetcode;
 
-import com.david.gupao.list.ListNode;
-
 /**
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
@@ -34,28 +32,28 @@ public class Question2 {
     }
 
     public static class Solution {
+
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
             //头结点
             ListNode head = new ListNode(-1);
             ListNode curr = head;
             int carrier = 0;
+            //两个链表全都遍历结束退出循环
             while (l1 != null || l2 != null) {
                 int x = l1 == null ? 0 : l1.val;
                 int y = l2 == null ? 0 : l2.val;
                 int sum = x + y + carrier;
                 carrier = sum / 10;
+                //组织结果链表
                 curr.next = new ListNode(sum % 10);
                 curr = curr.next;
-                if (l1 != null) {
-                    l1 = l1.next;
-                }
-                if (l2 != null) {
-                    l2 = l2.next;
-                }
+                //迭代
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
             }
-            if (carrier == 1) {
-                curr.next = new ListNode(carrier);
-            }
+            //最后一位有进位的话加一个节点
+            if (carrier == 1) curr.next = new ListNode(carrier);
+
             return head.next;
         }
     }
